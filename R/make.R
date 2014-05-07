2d79173375e4beb95bde1e5cef76d23353fca7c9
Karl W Broman <kbroman@biostat.wisc.edu>
2014-05-07 08:40:24 +0000

    version 0.48-2

---
+++ R/make.R
@@ -0,0 +1,34 @@
+#' Run make within a package directory
+#'
+#' Run make within a package directory
+#'
+#' @param pkg package description, can be path or package name.
+#' See \code{\link[devtools]{as.package}} for more information
+#'
+#' @param makefile File name of makefile
+#'
+#' @param quiet If TRUE suppresses output from this function
+#'
+#' @export
+#' @return Exit value from \code{\link[base]{system}} with \code{intern=FALSE}
+#'
+#' @examples
+#' \dontrun{make() # run make within working directory
+#' make("/path/to/mypackage") # run make within /path/to/mypackage
+#' }
+#'
+#' @seealso \code{\link[devtools]{load_all}}
+#' @keywords utilities
+make <-
+function(pkg = ".", makefile="Makefile", quiet=FALSE)
+{
+  pkg <- devtools::as.package(pkg)
+
+  if(!quiet) message("Making ", pkg$package)
+
+  fileflag <- ifelse(makefile == "" || makefile=="Makefile" || makefile == "makefile", "", paste("-f", makefile))
+
+  system(paste("cd", pkg$path, "; make", fileflag),
+         ignore.stdout=quiet, ignore.stderr=quiet,
+         intern=FALSE)
+}
--- src/runningmean.c
+++ src/runningmean.c
@@ -1,21 +1,21 @@
 /**********************************************************************
- * 
+ *
  * runningmean.c
  *
- * copyright (c) 2006-2011, Karl W Broman
+ * copyright (c) 2006-2014, Karl W Broman
  *
- * last modified Dec, 2011
+ * last modified Mar, 2014
  * first written Dec, 2006
  *
  *     This program is free software; you can redistribute it and/or
  *     modify it under the terms of the GNU General Public License,
  *     version 3, as published by the Free Software Foundation.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but without any warranty; without even the implied warranty of
  *     merchantability or fitness for a particular purpose.  See the GNU
  *     General Public License, version 3, for more details.
- * 
+ *
  *     A copy of the GNU General Public License, version 3, is available
  *     at http://www.r-project.org/Licenses/GPL-3
  *
@@ -25,7 +25,7 @@
  * Also for calculating a running ratio.
  *
  * Contains: runningmean, R_runningmean, runningratio, R_runningratio
- *  
+ *
  **********************************************************************/
 
 #include <math.h>
@@ -52,22 +52,21 @@
  * We assume that pos and resultpos are both sorted (lo to high)
  *
  **********************************************************************/
-void runningmean(int n, double *pos, double *value, 
-		 int n_result, 
-		 double *resultpos, double *result, 
-		 double window, int method)
+void runningmean(int n, double *pos, double *value,
+                 int n_result,
+                 double *resultpos, double *result,
+                 double window, int method)
 {
   int lo, ns;
   int i, j;
   double *work3, work4;
-  
-  
+
   if(method==3)
     work3 = (double *)R_alloc(n, sizeof(double));
 
   window /= 2.0;
 
-  lo=0; 
+  lo=0;
   for(i=0; i<n_result; i++) {
 
     R_CheckUserInterrupt(); /* check for ^C */
@@ -78,37 +77,42 @@ void runningmean(int n, double *pos, double *value,
       else if(pos[j] > resultpos[i]+window) break;
       else {
 
-	if(method==1 || method==2 || method==4)
-	  result[i] += value[j];
-	if(method==3) 
-	  work3[ns] = value[j];
-	if(method==4)
-	  work4 += (value[j]*value[j]);
+        if(method==1 || method==2 || method==4)
+          result[i] += value[j];
+        if(method==3)
+          work3[ns] = value[j];
+        if(method==4)
+          work4 += (value[j]*value[j]);
 
-	ns++;
+        ns++;
       }
     }
+
     if(ns==0 || (method==4 && ns==1)) result[i] = NA_REAL;
     else {
       if(method==2) result[i] /= (double)ns;
       if(method==3) {
-	R_rsort(work3, ns);
-	if(ns % 2) 
-	  result[i] = work3[(ns-1)/2];
-	else /* even */
-	  result[i] = (work3[ns/2-1]+work3[ns/2])/2.0;
+        R_rsort(work3, ns);
+        if(ns % 2)
+          result[i] = work3[(ns-1)/2];
+        else /* even */
+          result[i] = (work3[ns/2-1]+work3[ns/2])/2.0;
       }
-      if(method==4) 
-	result[i] = sqrt((work4 - result[i]*result[i]/(double)ns)/((double)(ns-1)));
+
+      if(method==4) { /* SD */
+        result[i] = (work4 - result[i]*result[i]/(double)ns)/(double)(ns-1);
+        if(result[i] < 0) result[i] = 0.0; /* handle potential round-off error by just thresholding to 0 */
+        else result[i] = sqrt(result[i]);
+      }
     }
   }
 
 }
 
 /* wrapper for R */
-void R_runningmean(int *n, double *pos, double *value, 
-		   int *n_result, double *resultpos, double *result, 
-		   double *window, int *method)
+void R_runningmean(int *n, double *pos, double *value,
+                   int *n_result, double *resultpos, double *result,
+                   double *window, int *method)
 {
   runningmean(*n, pos, value, *n_result, resultpos, result, *window, *method);
 }
@@ -122,15 +126,15 @@ void R_runningmean(int *n, double *pos, double *value,
  * We assume that pos and resultpos are sorted (lo to high)
  **********************************************************************/
 void runningratio(int n, double *pos, double *numerator, double *denominator,
-		  int n_result, double *resultpos, double *result, double window)
+                  int n_result, double *resultpos, double *result, double window)
 {
   int lo, ns;
   int i, j;
   double top, bottom;
-  
+
   window /= 2.0;
 
-  lo=0; 
+  lo=0;
   for(i=0; i<n_result; i++) {
 
     R_CheckUserInterrupt(); /* check for ^C */
@@ -140,9 +144,9 @@ void runningratio(int n, double *pos, double *numerator, double *denominator,
       if(pos[j] < resultpos[i]-window) lo = j+1;
       else if(pos[j] > resultpos[i]+window) break;
       else {
-	top += numerator[j];
-	bottom += denominator[j];
-	ns++;
+        top += numerator[j];
+        bottom += denominator[j];
+        ns++;
       }
     }
 
@@ -155,7 +159,7 @@ void runningratio(int n, double *pos, double *numerator, double *denominator,
 
 /* wrapper for R */
 void R_runningratio(int *n, double *pos, double *numerator, double *denominator,
-		    int *n_result, double *resultpos, double *result, double *window)
+                    int *n_result, double *resultpos, double *result, double *window)
 {
   runningratio(*n, pos, numerator, denominator, *n_result, resultpos, result, *window);
 }
